b463803aa46fc22a24a89bf3a9f7cc450a9063b8
Kema-dev <01sludge_coaches@icloud.com>
2022-01-29 14:26:37 +0000

    Shortest added, Grace ongoing, trying to understand diff bug

---
+++ Grace/src/Grace.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+#include<fcntl.h>
+#define fun main
+int fun(a){dprintf(open("Grace_kid.c",O_WRONLY|O_CREAT|O_TRUNC,0644),a="#include<stdio.h>%c#include<fcntl.h>%c#define fun main%cint fun(a){dprintf(open(%cGrace_kid.c%c,O_WRONLY|O_CREAT|O_TRUNC,0644),a=%c%s%c,13,13,13,34,34,34,a,34);}",13,13,13,34,34,34,a,34);}
